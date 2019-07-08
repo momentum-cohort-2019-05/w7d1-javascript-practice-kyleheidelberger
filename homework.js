bb7bdcf3536e82e8f9701b4a0ef3637d285f5bb0
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-07-08 13:40:30 -0400

    complete tasks 1-12

--- homework.js
+++ homework.js
@@ -1,26 +1,52 @@
 // 1. Set the variable `givenName` to the string "Addison".
+let givenName = "Addison"
 
 // 2. Set candies equal to 20, people to 6, and leftover equal
 // to the remainder of dividing 20 by 6.
+let candies = 20;
+let people = 6;
+let leftover = 20 % 6;
 
 // 3. Create a function called greeting that returns "Hello, <name>!",
 // where <name> is the name given as an argument to the function.
-
+function greeting(name) {
+    return `Hello, ${name}!`
+}
 // 4. Create a function called isOdd that, given a number, will
 // return true if it is odd and false if it is not. An odd number is a
 // number which, when divided by 2, has a remainder of 1 or -1.
-
+function isOdd(number) {
+    if (number % 2 === 1 || number % 2 === -1) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 // 5. Create a function called isEven that, given a number, will
 // return true if it is even and false if it is not. An even number is a
 // number which, when divided by 2, has a remainder of 0.
-
+function isEven(number) {
+    if (number % 2 === 0) {
+        return true
+    }
+    else {
+        return false
+    }
+}
 // 6. Create a function called fahrenheitToCelsius that takes a
 // Fahrenheit temperature as an argument and returns the
 // temperature in Celsius.
+function fahrenheitToCelsius(temperature) {
+    return (temperature - 32) * 5 / 9
+}
 
 // 6. Create a function called celsiusToFahrenheit that takes a
 // Celsius temperature as an argument and returns the
 // temperature in Fahrenheit.
+function celsiusToFahrenheit(temperature) {
+    return temperature * 9 / 5 + 32
+}
 
 // 7. Create a function called fahrenheitToKelvin that takes a
 // Fahrenheit temperature as an argument and returns the
@@ -28,10 +54,22 @@
 // fahrenheitToCelsius function.
 // Absolute zero (0 K) is equivalent to −273.15 C.
 // 1 degree Kelvin equals 1 degree Celsius.
+function fahrenheitToKelvin(temperature) {
+    temperature = fahrenheitToCelsius(temperature)
+    return temperature + 273.15
+}
 
 // 8. Create a function called lesser that takes two numbers as
 // arguments and returns the lesser of them. This function should
 // use an if/else statement.
+function lesser(number, othernumber) {
+    if (number < othernumber) {
+        return number
+    }
+    else {
+        return othernumber
+    }
+}
 
 // 9. Create a function called multigreeting that takes a name
 // and a language code and returns a version of "Hello, <name>!"
@@ -44,15 +82,65 @@
 // eo - Saluton, <name>!
 //
 // If any other language code is used, return nothing.
-
+function multigreeting(name, language) {
+    if (language === "en") {
+        return `Hello, ${name}!`
+    }
+    else if (language === "es") {
+        return `¡Hola, ${name}!`
+    }
+    else if (language === "fr") {
+        return `Bonjour, ${name}!`
+    }
+    else if (language === "eo") {
+        return `Saluton, ${name}!`
+    }
+    else {
+    }
+}
 // 10. Create a function called "sum" that takes an array of numbers and
 // returns the sum of those numbers.
+function sum(numbers) {
+    let total = 0
+    for (let number of numbers) {
+        total = total + number
+    }
+    return total
+}
 
 // 11. Create a function called "average" that takes an array of numbers
 // and returns the average of those numbers.
+function average(numbers) {
+    if (numbers.length < 1) {
+        return undefined
+    }
+    else {
+        let total = 0
+        for (let number of numbers) {
+            total = total + number
+        }
+        return total / numbers.length
+    }
+
+}
 
 // 12. Create a function called "minimum" that takes an array of numbers and
 // returns the smallest number in that array.
+function minimum(numbers) {
+
+    if (numbers.length < 1) {
+        return undefined
+    }
+    else {
+        let min = numbers[0];
+        for (let number of numbers) {
+            if (number < min) {
+                min = number
+            }
+        }
+        return min
+    }
+}
 
 // 13. There are many techniques to sort arrays in programming. Your programming
 // language will likely include the ability to do this. We are going to
